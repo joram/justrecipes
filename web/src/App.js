69dae474028e36780e979f426a714923a916b1d0
John Oram <john@oram.ca>
2020-09-25 11:43:01 -0700

    cleanup

--- web/src/App.js
+++ web/src/App.js
@@ -1,45 +1,59 @@
 import React from 'react';
 import './App.css';
-import {List, Rail, Grid, Segment} from 'semantic-ui-react'
+import {Grid, List, Menu, Segment, Sidebar} from 'semantic-ui-react'
 
 
 class Ingredient extends React.Component {
   render(){
-    return <List.Item>
-      <List.Content>
-        <List.Header>{this.props.ingredient.name}</List.Header>
-        <List.Description>
-          {this.props.ingredient.spoken}
-        </List.Description>
-      </List.Content>
-    </List.Item>
+    return <Menu.Item as='a'>
+      {this.props.ingredient.name}
+      <br/>
+      ({this.props.ingredient.spoken})
+    </Menu.Item>
+
   }
 }
 
 
 class Ingredients extends React.Component {
   render(){
+
+    let i = 0;
     let ingredients = []
-    console.log(this.props.ingredients)
     this.props.ingredients.forEach(ingredient => {
-      ingredients.push(<Ingredient ingredient={ingredient} />)
+      ingredients.push(<Ingredient ingredient={ingredient} key={"ingredient_"+i}/>)
+      i += 1
     })
 
-    return (<List horizontal={false}>
-          {ingredients}
-      </List>
-    )
+    return (<Sidebar
+      as={Menu}
+      icon='labeled'
+      vertical
+      visible={true}
+    >
+      <br/>
+      <List.Header><h3>Ingredients</h3></List.Header>
+      {ingredients}
+    </Sidebar>)
   }
 }
 
 
 class Instructions extends React.Component {
   render() {
+    let i = 1;
     let steps = []
     this.props.instructions.forEach(step => {
-      steps.push(<List.Item>{step}</List.Item>)
+      steps.push(<List.Item key={"instruction_"+i}>
+
+        <List.Content>
+          {i}) {step}
+        </List.Content>
+      </List.Item>)
+      i += 1;
     })
-    return <List>
+    return <List divided relaxed>
+      <List.Header><h3>Instructions</h3></List.Header>
       {steps}
     </List>
   }
@@ -59,8 +73,8 @@ class App extends React.Component {
 
   componentDidMount() {
     let host = "https://recipes.oram.ca"
-    if(window.location.hostname==="localhost")
-      host = "http://localhost:5000"
+    // if(window.location.hostname==="localhost")
+    //   host = "http://localhost:5000"
 
     fetch(`${host}/api/v0/recipe/recipe_da2ddeea30c98822dbfa6182dc4f465a100e919d438691913bc8a1c7`)
     .then(res => res.json())
@@ -85,11 +99,7 @@ class App extends React.Component {
 
       <Grid centered columns={2}>
           <Grid.Column>
-              <Rail position='left'>
-                <Segment>
-                  <Ingredients ingredients={ingredients} />
-                </Segment>
-              </Rail>
+              <Ingredients ingredients={ingredients} />
               <Segment>
                  <Instructions instructions={instructions} />
               </Segment>
