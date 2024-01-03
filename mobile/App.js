ad864361bb7307aa84c341921979b21e583ee3d6
John Oram <john@oram.ca>
2024-01-03 07:27:09 -0800

    start of mobile app

--- mobile/App.js
+++ mobile/App.js
@@ -1,20 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {FlatList, Keyboard, ScrollView, StyleSheet, Text, View} from 'react-native';
+import TaskInputField from "./components/taskInputField";
+import {useState} from "react";
+import TaskItem from "./components/taskItem";
 
 export default function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (task) => {
+    if (task == null) return;
+    setTasks([...tasks, task]);
+    Keyboard.dismiss();
+  }
+
+  const deleteTask = (deleteIndex) => {
+    setTasks(tasks.filter((value, index) => index !== deleteIndex));
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <Text style={styles.heading}>Recipe Shopping List</Text>
+        <ScrollView style={styles.scrollView}>
+          {
+            tasks.map((task, index) => {
+              return (
+                  <View key={index} style={styles.taskContainer}>
+                    <TaskItem index={index + 1} task={task} deleteTask={() => deleteTask(index)}/>
+                  </View>
+              );
+            })
+          }
+        </ScrollView>
+        <TaskInputField addTask={addTask}/>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#1E1A3C',
   },
+  heading: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: '600',
+    marginTop: 30,
+    marginBottom: 10,
+    marginLeft: 20,
+  },
+  scrollView: {
+    marginBottom: 70,
+  },
+  taskContainer: {
+    marginTop: 20,
+  }
 });
---
+++ mobile/README.md
@@ -0,0 +1,4 @@
+```bash
+export ANDROID_HOME="/usr/lib/android-sdk/"
+export PATH="${PATH}:${ANDROID_HOME}tools/:${ANDROID_HOME}platform-tools/"
+```
